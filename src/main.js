3ade7d6e8659af1ed9cb0198beb07690e50ffd08
tnguye37 <tnguye37@ncsu.edu>
2026-01-04 16:27:06 -0500

    Created mobile menu, added js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden'); //toggle will do the opposite
+});
