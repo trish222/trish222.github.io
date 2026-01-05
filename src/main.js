e4d04187878a0af120d6490bce1fa42bae477c39
tnguye37 <tnguye37@ncsu.edu>
2026-01-05 18:42:49 -0500

    menu dropdown smooth transition added

--- src/main.js
+++ src/main.js
@@ -4,5 +4,14 @@ const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
 menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden'); //toggle will do the opposite
+  
+  // toggle the 'off' classes
+  mobileMenu.classList.toggle('invisible');
+  mobileMenu.classList.toggle('opacity-0');
+  mobileMenu.classList.toggle('scale-95');
+  
+  // toggle the 'on' classes
+  mobileMenu.classList.toggle('visible');
+  mobileMenu.classList.toggle('opacity-100');
+  mobileMenu.classList.toggle('scale-100');
 });
