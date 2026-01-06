cf0954b465e1f3d317090265da7c90fa1cfa4c48
tnguye37 <tnguye37@ncsu.edu>
2026-01-05 20:02:18 -0500

    working on theme toggle button

--- src/main.js
+++ src/main.js
@@ -1,17 +1,42 @@
 import './style.css';
 
+/* ==========================================
+   CONSTANTS
+   ========================================== */
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
+const html = document.documentElement;
+const themeToggleBtn = document.getElementById('theme-toggle');
 
-menuBtn.addEventListener('click', () => {
-  
-  // toggle the 'off' classes
-  mobileMenu.classList.toggle('invisible');
-  mobileMenu.classList.toggle('opacity-0');
-  mobileMenu.classList.toggle('scale-95');
-  
-  // toggle the 'on' classes
-  mobileMenu.classList.toggle('visible');
-  mobileMenu.classList.toggle('opacity-100');
-  mobileMenu.classList.toggle('scale-100');
-});
+/* ==========================================
+   FUNCTIONS
+   ========================================== */
+
+const toggleTheme = () => {
+    html.classList.toggle('dark');
+    const isDark = html.classList.contains('dark');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+};
+
+/* ==========================================
+   EVENT LISTENERS
+   ========================================== */
+
+if (menuBtn && mobileMenu) {
+    menuBtn.addEventListener('click', () => {
+        // toggle the 'off' classes
+        mobileMenu.classList.toggle('invisible');
+        mobileMenu.classList.toggle('opacity-0');
+        mobileMenu.classList.toggle('scale-95');
+        
+        // toggle the 'on' classes
+        mobileMenu.classList.toggle('visible');
+        mobileMenu.classList.toggle('opacity-100');
+        mobileMenu.classList.toggle('scale-100');
+    });
+    
+}
+
+if (themeToggleBtn) {
+    themeToggleBtn.addEventListener('click', toggleTheme);
+}
